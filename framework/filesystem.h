a9225735fb055480d2ffbd80d22e141bb9910d53
Jonathan Hamilton <jtrhamilton@gmail.com>
2020-05-11 15:32:13 -0700

    Pull std::filesystem ::fs alias to OpenApoc::fs

--- framework/filesystem.h
+++ framework/filesystem.h
@@ -3,13 +3,17 @@
 #if defined(USE_STD_FILESYSTEM)
 
 #include <filesystem>
+namespace OpenApoc
+{
 namespace fs = std::filesystem;
-
+}
 #elif defined(USE_BOOST_FILESYSTEM)
 
 #include <boost/filesystem.hpp>
+namespace OpenApoc
+{
 namespace fs = boost::filesystem;
-
+}
 #else
 #error No filesystem implementation
 #endif
