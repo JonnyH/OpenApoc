10ccd9ab3304a9aab56351c2d8d5b04344056f6a
Jonathan Hamilton <jtrhamilton@gmail.com>
2024-03-03 16:57:44 -0800

    Prep for fmt::format

--- library/strings_format.h
+++ library/strings_format.h
@@ -10,6 +10,10 @@ template <typename... Args> static UString format(const UStringView fmt, Args &&
 {
 	return fmt::sprintf(fmt, std::forward<Args>(args)...);
 }
+template <typename... Args> static UString format2(const UStringView fmt, Args &&...args)
+{
+	return fmt::format(fmt, std::forward<Args>(args)...);
+}
 
 UString tr(const UString &str, const UString domain = "ufo_string");
 
